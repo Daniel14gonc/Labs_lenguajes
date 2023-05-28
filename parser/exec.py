d1cf2c7acc6e4c2218124211805a7fc601617545
Daniel <gon20293@uvg.edu.gt>
2023-05-28 16:48:07 -0600

    chore: delete parser

--- parser/exec.py
+++
@@ -1,7 +0,0 @@
-from lexer import tokenizer
-from parser import slr
-
-input_file = input("Ingrese el nombre del archivo de codigo fuente> ")
-tokenizer.read_source_code(input_file)
-tokenizer.initialize_token_recognition()
-print(slr.parse(tokenizer))
--- parser/exec_java.py
+++
@@ -1,7 +0,0 @@
-from lexer_java import tokenizer
-from parser_java import slr
-
-input_file = input("Ingrese el nombre del archivo de codigo fuente> ")
-tokenizer.read_source_code(input_file)
-tokenizer.initialize_token_recognition()
-print(slr.parse(tokenizer))
